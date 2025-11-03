7e457bd5b8df422ea428560bb4356b1007ad9417
matheosbrainer-hub <matheosbrainer@gmail.com>
2025-11-03 22:08:45 +0000

    Delete components directory

--- components/DarkToggle.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-export default function DarkToggle({ theme, toggleTheme }: any){
-  return (
-    <button onClick={toggleTheme} className="btn ghost" style={{padding:'8px 10px'}}>{theme==='dark' ? 'Light' : 'Dark'} mode</button>
-  )
-}
